bcba7faf1d9b571b183958598d91ca28301dc54f
yuan0936 <yuan@gocharm.com.tw>
2014-12-17 17:27:33 +0800

    fixed

--- themes/todoList/blocks/views/index/index.js
+++ themes/todoList/blocks/views/index/index.js
@@ -2,6 +2,11 @@ ctrl.startup = function() {
 	if ('<%=bi.client.category%>' === 'mobile' || '<%=bi.client.category%>' === 'tablet') { 		
 		ctrl.sel(".content").addClass('mobile');			
 	}
-	
+	liClick();
 };
 
+function liClick() {
+    ctrl.sel('.itemNum').click(function() {	
+		$(".itemNum").remove()
+	}
+};
