c367d8ac621a153f4c7382cfa88c89f96d81b950
yuan0936 <yuan@gocharm.com.tw>
2014-12-27 14:38:13 +0800

    insert

---
+++ themes/typeTablet/blocks/modules/type2/index.js
@@ -0,0 +1,26 @@
+exports.run = function(inData, callback)  {
+	var  rtnData = {errCode: 0};
+	rtnData.value = {
+		list: [
+			{					
+				img: '../type/400x300.gif',
+				title: 'Welcome to the coServ',
+				body: 'The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.',
+				p: 'It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is.The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.'				
+			},
+			{					
+				img: '../type/400x300.gif',
+				title: 'Welcome to the coServ',
+				body: 'The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.',
+				p: 'It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is.The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.'				
+			},
+			{					
+				img: '../type/400x300.gif',
+				title: 'Welcome to the coServ',
+				body: 'The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.',
+				p: 'It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is.The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.'				
+			}
+		]
+	};
+	callback( rtnData );
+};
